e1bb8a6688271433fad093f630b79cc481dda25e
Artem <artem@webart-tech.ru>
2018-02-07 10:57:24 +0700

    Поправил зависимость

--- providers/etoro/data_classess.py
+++ providers/etoro/data_classess.py
@@ -4,16 +4,26 @@ from dataclasses import dataclass
 
 
 @dataclass
+class InstrumentList:
+    instrument_id: int
+    instrument_display_name: str
+    stock_industry_id: int
+    symbol_full: str
+
+
+@dataclass
 class Instrument:
     id: int
     name: str
     series: pd.Series
+
 
 @dataclass
 class InstrumentHistory:
     id: List[int]
     name: List[str]
     series: List[pd.Series]
+
 
 @dataclass
 class SpreadInstrument:
--- providers/etoro/methods.py
+++ providers/etoro/methods.py
@@ -1,10 +1,10 @@
 import json
 from datetime import datetime
-from typing import Tuple, List
+from typing import Tuple, List, Dict
 
 import pandas as pd
 from statsmodels.tsa.stattools import adfuller, coint
-# from matplotlib import pyplot
+from matplotlib import pyplot
 import numpy
 import statsmodels.formula.api as sm
 
@@ -12,7 +12,8 @@ from providers.abc import BaseProvider
 from providers.etoro import helpers
 from config import login_data
 from logger.logger import logger
-from providers.etoro.data_classess import Instrument, InstrumentHistory, SpreadInstrument
+from providers.etoro.data_classess import (Instrument, InstrumentHistory,
+                                           SpreadInstrument, InstrumentList)
 
 
 class EtoroProvider(BaseProvider):
@@ -54,23 +55,34 @@ class EtoroProvider(BaseProvider):
         await self.login()
         instruments = await self.get_instruments()
         rows = []
-        for num, instrument in enumerate(json.loads(instruments)["InstrumentDisplayDatas"]):
-            history = await self.get_history(instrument["InstrumentID"])
+        for num, instrument in enumerate(instruments):
+            history = await self.get_history(instrument.instrument_id)
             is_adfuller, series = self.check_adfuller(json.loads(history))
             if is_adfuller:
-                rows.append(Instrument(id=instrument["InstrumentID"], name=instrument["InstrumentDisplayName"],
-                                       series=series))
-            if num > 2:
+                rows.append(Instrument(id=instrument.instrument_id,
+                                       name=instrument.instrument_display_name, series=series))
+            if num >= 5:
                 break
         rows = self.check_coin(rows)
-        print(rows)
+        for r in rows:
+            print(r.name[0], r.name[1], r.spread)
 
-    async def get_instruments(self) -> dict:
+    async def get_instruments(self) -> List[InstrumentList]:
+        return_data = []  # type: List[InstrumentList]
         url = 'https://api.etorostatic.com/sapi/instrumentsmetadata/V1.1/instruments'
-        return await self.get(url, self.default_header)
+        instruments = await self.get(url, self.default_header)
+        for d in json.loads(instruments)["InstrumentDisplayDatas"]:
+            if d.get("StocksIndustryID", None) is None:
+                continue
+            return_data.append(InstrumentList(instrument_id=d["InstrumentID"],
+                                              instrument_display_name=d["InstrumentDisplayName"],
+                                              stock_industry_id=d["StocksIndustryID"],
+                                              symbol_full=d["SymbolFull"]))
+        return return_data
 
     async def get_history(self, instrument_id: int=None, count=1000):
-        url = 'https://candle.etoro.com/candles/desc.json/ThirtyMinutes/{}/{}'.format(count, instrument_id)
+        url = 'https://candle.etoro.com/candles/desc.json/ThirtyMinutes/{}/{}'.format(count,
+                                                                                      instrument_id)
         # url = 'https://candle.etoro.com/candles/desc.json/FiveMinutes/1000/1'
         data = await self.get(url, self.default_header)
         return data
@@ -94,25 +106,29 @@ class EtoroProvider(BaseProvider):
     def check_coin(history: List[Instrument], cutoff: float=0.05) -> List[SpreadInstrument]:
         return_data = []  # type: List[SpreadInstrument]
         l_history = []  # type: List[InstrumentHistory]
-
+        is_checked = {}  # type: Dict[str]bool
         for key_h1, h1 in enumerate(history):
             for key_h2, h2 in enumerate(history):
+                # проверка, чтобы два раза не дублировались данные
+                direct_name = '{}{}'.format(h2.name, h1.name)
+                revert_name = '{}{}'.format(h1.name, h2.name)
+                if is_checked.get(direct_name, None) is not None or is_checked.get(revert_name, None) is not None:
+                    continue
+                is_checked[direct_name] = True
+                is_checked[revert_name] = True
+
                 if key_h1 == key_h2:
                     continue
                 data = coint(h1.series, h2.series)  # type: List
                 if data[1] > cutoff:
-                    l_history.append(InstrumentHistory(series=[h1.series, h2.series], id=[h1.id, h2.id],
-                                                       name=[h1.name, h2.name]))
+                    l_history.append(InstrumentHistory(series=[h1.series, h2.series],
+                                                       id=[h1.id, h2.id], name=[h1.name, h2.name]))
 
         for row in l_history:
-            h1_coord = []
-            h2_coord = []
             y1 = []
             y2 = []
             x = []
-            for key, r in enumerate(row.series[0]):
-                h1_coord.append([key, row.series[0][key]])
-                h2_coord.append([key, row.series[1][key]])
+            for key, _ in enumerate(row.series[0]):
                 y1.append(row.series[0][key])
                 y2.append(row.series[1][key])
                 x.append(numpy.divide(key, 2))
@@ -120,10 +136,10 @@ class EtoroProvider(BaseProvider):
             df["X"] = x
             df["Y1"] = y1
             df["Y2"] = y2
-            results = sm.ols(formula='X ~ Y1 + Y2', data=df).fit()
+            results = sm.ols(formula='Y1 ~ Y2', data=df).fit()
             y = [v for v in results.resid]
-            # pyplot.plot(x, y1)
-            # pyplot.plot(x, y2)
+            #pyplot.plot(x, y1)
+            #pyplot.plot(x, y2)
             # pyplot.plot(x, y)
             # pyplot.show()
             return_data.append(SpreadInstrument(spread=y[-1], name=row.name, id=row.id))
